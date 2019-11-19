5550e26b8d2b159710fccf6e0323b9235ae1d460
Adrian <adriandltd@gmail.com>
2019-11-19 14:21:30 -0600

    Put in game code to start messing with

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//references: https://colorlib.com/etc/lf/Login_v16/index.html
 const { User, Highscore, Streaks } = require('./models');
 
 const express = require('express');
@@ -19,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // set up session (in-memory storage by default)
 app.use(session({ secret: "mysupersecret" }));
 
+const http = require('http').Server(app);
 
 // setup handlebars and the view engine for res.render calls
 // (more standard to use an extension like 'hbs' rather than
@@ -38,6 +38,95 @@ app.engine('html', hbs({
 
 // setup static file service
 app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/game', function (req, res) {
+  res.render('game_pg.html');
+});
+
+let port = 5000;
+
+http.listen(port, function () {
+  console.log(`Tic-tac-toe game server running on port ${port}`);
+});
+
+
+const io = require('socket.io')(http);
+
+var players = {},
+  unmatched;
+
+function joinGame(socket) {
+
+  // Add the player to our object of players
+  players[socket.id] = {
+
+      // The opponent will either be the socket that is
+      // currently unmatched, or it will be null if no
+      // players are unmatched
+      opponent: unmatched,
+
+      // The symbol will become 'O' if the player is unmatched
+      symbol: 'X',
+
+      // The socket that is associated with this player
+      socket: socket
+  };
+
+  // Every other player is marked as 'unmatched', which means
+  // there is no another player to pair them with yet. As soon
+  // as the next socket joins, the unmatched player is paired with
+  // the new socket and the unmatched variable is set back to null
+  if (unmatched) {
+      players[socket.id].symbol = 'O';
+      players[unmatched].opponent = socket.id;
+      unmatched = null;
+  } else {
+      unmatched = socket.id;
+  }
+}
+
+// Returns the opponent socket
+function getOpponent(socket) {
+  if (!players[socket.id].opponent) {
+      return;
+  }
+  return players[
+      players[socket.id].opponent
+  ].socket;
+}
+
+io.on('connection', function (socket) {
+  console.log("Connection established...", socket.id);
+  joinGame(socket);
+
+  // Once the socket has an opponent, we can begin the game
+  if (getOpponent(socket)) {
+      socket.emit('game.begin', {
+          symbol: players[socket.id].symbol
+      });
+      getOpponent(socket).emit('game.begin', {
+          symbol: players[getOpponent(socket).id].symbol
+      });
+  }
+
+  // Listens for a move to be made and emits an event to both
+  // players after the move is completed
+  socket.on('make.move', function (data) {
+      if (!getOpponent(socket)) {
+          return;
+      }
+      console.log("Move made by : ", data);
+      socket.emit('move.made', data);
+      getOpponent(socket).emit('move.made', data);
+  });
+
+  // Emit an event to the opponent when the player leaves
+  socket.on('disconnect', function () {
+      if (getOpponent(socket)) {
+          getOpponent(socket).emit('opponent.left');
+      }
+  });
+});
 
 
 app.get('/login', (req, res) => {
